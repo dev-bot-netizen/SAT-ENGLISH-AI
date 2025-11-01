a3767aaadb6ed04b05d3a24944012d8f13c360c4
Akshaj Goel <goelakshaj@gmail.com>
2025-11-01 11:53:54 +0530

    Update TrustSection.tsx

--- src/components/landing/TrustSection.tsx
+++ src/components/landing/TrustSection.tsx
@@ -98,7 +98,7 @@ const TrustSection: React.FC = () => {
 
   return (
     <section ref={component} className="relative overflow-hidden py-20 sm:py-24 lg:py-32 bg-brand-indigo/50">
-      <BackgroundGlowWords />
+      <BackgroundGlowWords wordCount={15} />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center relative z-10">
         <div className="flex flex-col sm:flex-row items-center justify-center gap-2">
             <div className="flex">
